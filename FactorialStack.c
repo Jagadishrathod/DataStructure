2ba852e5d3a988d8384af86f1cec352ce2af49a4
Rathod Jagadish <46041625+Jagadishrathod@users.noreply.github.com>
2021-06-26 11:35:18 +0530

    Add files via upload

---
+++ FactorialStack.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int x;
+	struct node *nptr;
+};
+
+void push(int x);
+int pop();
+
+struct node *top = NULL;
+
+void main(){
+	printf("-----Factorial using Stack-----\n");	
+	printf("Enter the value of n: ");
+	int n;
+	scanf("%d",&n);
+	push(1);
+	for(int i = 2;i < n + 1;i++){
+		int i2 = pop();
+		i2 = (int)i*i2;
+		push(i2);
+	}
+	int ans = pop();
+	printf("Factorial of n! : %d",ans);
+	printf("\n*******************************\n\n");
+}
+
+void push(int x){
+	struct node *temp = malloc(sizeof(struct node));
+	if(temp==NULL)
+		printf("No memory.\n");
+	else {
+		if(top==NULL){
+			top=temp;
+			temp->x = x;
+			temp->nptr = NULL;
+		}
+		else {
+			temp->x = x;
+			temp->nptr = top;
+			top = temp;
+		}
+	}
+}
+
+int pop(){
+	int t=top->x;
+	if(top->nptr==NULL){
+		top=NULL;
+	}
+	else {
+		top=top->nptr;
+	}
+	return t;
+}
+
